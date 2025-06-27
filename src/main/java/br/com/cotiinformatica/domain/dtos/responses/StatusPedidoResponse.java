d1e1540a906b02126485c177d17c215e9552aa45
fncvizoni <fncvizoni@gmail.com>
2025-06-27 14:31:02 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/domain/dtos/responses/StatusPedidoResponse.java
@@ -0,0 +1,11 @@
+package br.com.cotiinformatica.domain.dtos.responses;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class StatusPedidoResponse {
+	private Integer id;
+	private String nome;
+}
+
